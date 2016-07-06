8591c506fe0f9483b8338a9272b631e2ff90e4ca
milkis37 <milkis37@naver.com>
2016-07-06 10:41:58 +0900

    test

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,7 +5,7 @@ int main()
   std::cout << "Hello World." << std::endl;
   std::cout << "Kiin Na" << std::endl;	
   std::cout << "Junsuk Lee" << std::endl;
-  std::cout << "enmi chan" << std::endl;
+  std::cout << "eunmi cheon" << std::endl;
 
   return 1;
 }
