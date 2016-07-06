25c00a383186df08272c5da30bdcfcb219527138
milkis37 <milkis37@naver.com>
2016-07-06 10:35:43 +0900

    enmi

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,6 +5,7 @@ int main()
   std::cout << "Hello World." << std::endl;
   std::cout << "Kiin Na" << std::endl;	
   std::cout << "Junsuk Lee" << std::endl;
+  std::cout << "enmi chan" << std::endl;
 
   return 1;
 }
